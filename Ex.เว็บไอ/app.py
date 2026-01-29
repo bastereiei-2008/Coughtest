7fca0621b88d8df7da4e9fff4ddc8afd0509f323
bastereiei-2008 <bastereiei@gmail.com>
2026-01-30 00:05:22 +0700

    Update app.py

--- Ex.เว็บไอ/app.py
+++ Ex.เว็บไอ/app.py
@@ -120,7 +120,7 @@ def reset():
 if st.session_state.page == 1:
     st.markdown('<div class="css-card">', unsafe_allow_html=True)
     st.title("ระบบคัดกรองโรคทางเดินหายใจในเด็กเบื้องต้น")
-    st.markdown("###CoughCareKids")
+    st.markdown("### CoughCareKids")
     st.write("---")
     c1, c2 = st.columns([1, 1])
     with c1:
@@ -252,6 +252,7 @@ elif st.session_state.page == 4:
     c1, c2 = st.columns(2)
     with c1: st.button("🏠 กลับหน้าหลัก", on_click=reset)
     st.markdown('</div>', unsafe_allow_html=True)
+
 
 
 
